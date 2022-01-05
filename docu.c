dd8b11e37c2e9c85cafb4d178526f3e05382ec93
sudeepa pal <sudeepapal@sudeepas-MacBook-Air.local>
2022-01-05 20:30:18 +0530

    Index

---
+++ docu.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float x=0.5;
+    if(x==0.5)
+    printf("IF");
+     else if(x==0.5f)
+        printf("ELSE IF");
+        else 
+        printf("ELSE");
+        return 0;
+}
