362a49340bc614d9e9499b0ad8ab9fbf78f2748a
Sudeepa <palsudeepa@gmail.com>
2022-01-05 22:05:53 +0530

    added hura file

---
+++ hura.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x, min, max;
+    scanf("%d%d",&max,&x);
+    
+    if(x>max)
+       max=x;
+       
+   else
+       min=x;
+       
+       return 0;
+}
